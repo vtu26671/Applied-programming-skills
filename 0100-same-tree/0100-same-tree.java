b30b4ec81b3bf0f4bb9381fbb3955e40341b6633
vtu26671 <vtu26671@veltech.edu.in>
2026-04-01 11:18:47 +0530

    Time: 0 ms (100%), Space: 42.5 MB (95.28%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -1,16 +1,16 @@
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        // both are null
+        // 1. Both are null: they match
         if (p == null && q == null) {
             return true;
         }
-
-        // one is null or values differ
+        
+        // 2. One is null or values differ: they don't match
         if (p == null || q == null || p.val != q.val) {
             return false;
         }
-
-        // check left and right
+        
+        // 3. Check descendants recursively
         return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
 }
